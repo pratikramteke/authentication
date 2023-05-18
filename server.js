a80f5968aa41bb1c2776e5ace51a62252f35fcba
Pratik Ramteke <pratikramteke222@gmail.com>
2023-05-18 12:53:33 +0530

    css added

--- server.js
+++ server.js
@@ -16,22 +16,29 @@ const isAuthenticated = async (req, res, next) => {
   if (token) {
     const decoded = jwt.verify(token, config.JWT_SECRET)
     req.user = await User.findById(decoded._id)
-    // console.log(req.user);
     next()
-  }
-  else {
-    res.render('login')
+  } else {
+    res.render("login")
   }
 }
 
 app.get("/", isAuthenticated, (req, res) => {
-  const {name} = req.user
+  const { name } = req.user
   res.render("dashboard", { name })
+})
+
+app.get("/signup", (req, res) => {
+  res.render("signup")
 })
 
 app.post("/signup", async (req, res) => {
   try {
     const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+      return res.json({ success: false, message: "fields should not be empty" })
+    }
+
     const isUserExist = await User.findOne({ email })
     if (isUserExist) {
       res.json({ success: false, message: "user already exists login please" })
@@ -42,9 +49,16 @@ app.post("/signup", async (req, res) => {
 
     const user = await User.create({ name, email, password: hashedPassword })
     user.password = undefined
-    res.json({ success: true, message: "user registered", user })
+    const token = jwt.sign({ _id: user._id }, config.JWT_SECRET)
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      expires: new Date(Date.now() +60 * 60 * 1000),
+    })
+    return res.redirect("/dashboard")
   } catch (error) {
     console.log(error)
+    throw new Error(error)
   }
 })
 
@@ -62,17 +76,18 @@ app.post("/login", async (req, res) => {
       if (isPasswordMatch) {
         isUserExist.password = undefined
 
-        const token = jwt.sign({_id:isUserExist._id},config.JWT_SECRET)
+        const token = jwt.sign({ _id: isUserExist._id }, config.JWT_SECRET)
 
         res.cookie("token", token, {
           httpOnly: true,
-          expires: new Date(Date.now() + 60 * 1000),
+          expires: new Date(Date.now() + 60 * 60 * 1000),
         })
         return res.redirect("/dashboard")
-        
       }
-      return res.json({ success: false, message: "incorrect email or password" })
-      
+      return res.json({
+        success: false,
+        message: "incorrect email or password",
+      })
     }
     return res.json({ success: false, message: "please signup first" })
   } catch (error) {
@@ -82,8 +97,8 @@ app.post("/login", async (req, res) => {
 })
 
 app.get("/dashboard", isAuthenticated, (req, res) => {
-  const {name} = req.user
-  res.render("dashboard",{name})
+  const { name } = req.user
+  res.render("dashboard", { name })
 })
 
 app.post("/logout", (req, res) => {
@@ -92,6 +107,11 @@ app.post("/logout", (req, res) => {
     expires: new Date(Date.now()),
   })
   res.redirect("/")
+})
+
+app.get("/users",isAuthenticated, async (req, res) => {
+  const user = await User.find()
+  res.send(user)
 })
 
 app.listen(config.PORT, () => {
